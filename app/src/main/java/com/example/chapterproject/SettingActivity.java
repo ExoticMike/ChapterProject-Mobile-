d85e5b99546a881e3519fb0d01e18fad0c3245cb
mikiyasmekasha <mikiyasmekasha12@gmail.com>
2025-02-04 18:21:54 -0500

    Created the Settings layout

--- app/src/main/java/com/example/chapterproject/SettingActivity.java
+++ app/src/main/java/com/example/chapterproject/SettingActivity.java
@@ -1,6 +1,8 @@
 package com.example.chapterproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,26 @@ public class SettingActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
     }
+    private void initContactButton() {
+        ImageButton ContactButton = findViewById(R.id.ContactButton);
+        ContactButton.setOnClickListener(v -> {
+            Intent listIntent = new Intent(SettingActivity.this, ContactListActivity.class);
+            listIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(listIntent);
+        });
+    }
+    private void initMapButton() {
+        ImageButton MapButton = findViewById(R.id.MapButton);
+        MapButton.setOnClickListener(v -> {
+            Intent listIntent = new Intent(SettingActivity.this, MapActivity.class);
+            listIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(listIntent);
+        });
+    }
+    private void initSettingButton() {
+        ImageButton ibSettings = findViewById(R.id.SettingButton);
+        ibSettings.setEnabled(false);
+        }
 }
