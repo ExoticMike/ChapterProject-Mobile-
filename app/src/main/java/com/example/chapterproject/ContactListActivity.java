383c09400aebd4c82e5d8ea9fcd3ed58f8f047bf
mikiyasmekasha <mikiyasmekasha12@gmail.com>
2025-02-18 19:54:00 -0500

    Adapter code modified to respond to an item click

--- app/src/main/java/com/example/chapterproject/ContactListActivity.java
+++ app/src/main/java/com/example/chapterproject/ContactListActivity.java
@@ -1,12 +1,19 @@
 package com.example.chapterproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class ContactListActivity extends AppCompatActivity {
 
@@ -19,6 +26,51 @@ public class ContactListActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        initContactButton();
+        initMapButton();
+        initSettingButton();
+
+        ContactDataSource ds = new ContactDataSource(this);
+        ArrayList<String> names;
+
+        try{
+            ds.open();
+            names = ds.getContactName();
+            ds.close();
+            RecyclerView contactList = findViewById(R.id.rvContacts);
+            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+            contactList.setLayoutManager(layoutManager);
+            ContactAdapter contactAdapter = new ContactAdapter(names);
+            contactList.setAdapter(contactAdapter);
+        }
+        catch (Exception e) {
+            Toast.makeText(this,"Error retrieving contacts", Toast.LENGTH_LONG).show();
+        }
+    }
+    private void initContactButton() {
+        ImageButton ContactButton = findViewById(R.id.ContactButton);
+        ContactButton.setOnClickListener(v -> {
+            Intent listIntent = new Intent(ContactListActivity.this, ContactListActivity.class);
+            listIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(listIntent);
+        });
+    }
+    private void initMapButton() {
+        ImageButton MapButton = findViewById(R.id.MapButton);
+        MapButton.setOnClickListener(v -> {
+            Intent listIntent = new Intent(ContactListActivity.this, MapActivity.class);
+            listIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(listIntent);
+        });
+    }
+    private void initSettingButton() {
+        ImageButton SettingButton = findViewById(R.id.SettingButton);
+        SettingButton.setOnClickListener(v -> {
+            Intent listIntent = new Intent(ContactListActivity.this, SettingActivity.class);
+            listIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(listIntent);
         });
     }
 }
